5d0b9f982017a4627dbde443e1a6118ab6a033ad
jzolo22 <jzolotarev@gmail.com>
2020-12-20 15:02:01 -0500

    done

---
+++ src/components/MovieShow.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const MovieShow = (props) => {
+
+    // console.log(props)
+
+    // function makeMovie() {
+    //     const id = parseInt(props.match.params.movieId)
+    //     console.log(id)
+    // }
+
+    return (
+        <div>
+            <h3>Movie Show Component!</h3>
+        </div>
+    )
+
+}
+
+export default MovieShow
